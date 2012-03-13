ee67c69b8843bea3dd23ee40cb12802c4a7d1da3
charlysisto <charlysisto@gmail.com>
2012-03-13 17:10:49 +0100

    documented resource_helpers

--- lib/generators/backbonify/resource_helpers.rb
+++ lib/generators/backbonify/resource_helpers.rb
@@ -6,26 +6,32 @@ module Backbonify
         "app/assets/javascripts"
       end
 
+      # => Perf.Models.Performance
       def model_namespace
         [js_app_name, "Models", class_name].join(".")
       end
 
+      # => performance
       def singular_model_name
         uncapitalize singular_name.camelize
       end
 
+      # => performances
       def plural_model_name
         uncapitalize(plural_name.camelize)
       end
 
+      # => Perf.Collections.Performances
       def collection_namespace
         [js_app_name, "Collections", plural_name.camelize].join(".")
       end
 
+      # => Perf.Views.Performances
       def view_namespace
         [js_app_name, "Views", plural_name.camelize].join(".")
       end
 
+      # => Perf.Routers.Performances
       def router_namespace
         [js_app_name, "Routers", plural_name.camelize].join(".")
       end
@@ -38,6 +44,7 @@ module Backbonify
         application_name.camelize
       end
 
+      # TODO : Add an option so "App" is default
       def application_name
         if defined?(Rails) && Rails.application
           Rails.application.class.name.split('::').first
@@ -46,6 +53,7 @@ module Backbonify
         end
       end
 
+      # uncapitalize("ABC") => aBC
       def uncapitalize(str)
         str[0, 1].downcase << str[1..-1]
       end
